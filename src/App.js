21eb62348ca1a13e210b953de56ee7bc5748d5e6
Prachi Ramesh Godase <prachigodase2@gmail.com>
2023-10-12 17:44:00 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import ProductList from './components/ProductList';
+function App() {
+  return (
+    <div>
+  <ProductList/>
+</div>
+  );
+}
+
+export default App;
